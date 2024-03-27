1f6dc0adab8581d71f4b3e3cab73424a3314dc89
SKhuzhaev <skhuzhaev@beeline.uz>
2024-03-27 17:35:05 +0500

    upd: Wal operations were added

--- src/main/java/com/university/universityapplication/constans/PostgresBufferMethods.java
+++ src/main/java/com/university/universityapplication/constans/PostgresBufferMethods.java
@@ -19,4 +19,32 @@ public final class PostgresBufferMethods {
             "SELECT {0}( '%s' )",
             PG_PREWARM
     );
+
+    /*
+    создает расширение для работы и аналитики буфера кэширования
+    */
+    public static final String CREATE_EXTENSION_FOR_BUFFER_READ = "CREATE EXTENSION pg_buffercache;";
+
+    /*
+    После перезапуска сервера должно пройти некоторое время, чтобы кеш «прогрелся» — набрал актуальные активно использующиеся данные.
+    Иногда может оказаться полезным сразу прочитать в кеш данные определенных таблиц, и для этого предназначено специальное расширение:
+
+    Раньше расширение могло только читать определенные таблицы в буферный кеш (или только в кеш ОС).
+    Но в версии PostgreSQL 11 оно получило возможность сохранять актуальное состояние кеша на диск
+    и восстанавливать его же после перезагрузки сервера. Чтобы этим воспользоваться,
+    надо добавить библиотеку в shared_preload_libraries и перезагрузить сервер.
+
+    Поле рестарта, если не менялось значение параметра pg_prewarm.autoprewarm, будет автоматически запущен фоновый процесс autoprewarm master,
+    который раз в pg_prewarm.autoprewarm_interval будет сбрасывать на диск список страниц,
+    находящихся в кеше (не забудьте учесть новый процесс при установке max_parallel_processes).
+    */
+    public static final String CREATE_EXTENSION_FOR_BUFFER_WARMING = """
+            CREATE EXTENSION IF NOT EXISTS pg_prewarm; -- создаем расширение
+            ALTER SYSTEM SET shared_preload_libraries = 'pg_prewarm'; -- меняем настройки кластера
+            """;
+
+    /*
+    возвращает данные о потреблении буфера для конкретной таблицы
+    */
+    public static final String SELECT_BUFFER_ANALYZE_FOR_TABLE = "SELECT * FROM pg_buffercache_v WHERE relname= '%s';";
 }
---
+++ src/main/java/com/university/universityapplication/constans/PostgresWalOperations.java
@@ -0,0 +1,27 @@
+package com.university.universityapplication.constans;
+
+/*
+https://www.postgresql.org/docs/13/sql-checkpoint.html
+https://www.postgresql.org/docs/13/wal-configuration.html
+
+Несмотря на то, что в названии присутствует слово “точка”, процесс
+записи грязных буферов происходит не одномоментно, иначе бы это подвесило
+наш серверный процесс, а продолжительно по времени. Настраивается в %
+между двумя контрольными точками, то есть процесс записи размывается по
+времени
+*/
+public final class PostgresWalOperations {
+    /*
+    A checkpoint is a point in the write-ahead log sequence at which all data files have been updated to reflect the information in the log.
+    All data files will be flushed to disk. Refer to Section 29.4 for more details about what happens during a checkpoint.
+
+    The CHECKPOINT command forces an immediate checkpoint when the command is issued,
+    without waiting for a regular checkpoint scheduled by the system (controlled by the settings in Section 19.5.2).
+    CHECKPOINT is not intended for use during normal operation.
+
+    If executed during recovery, the CHECKPOINT command will force a restartpoint rather than writing a new checkpoint.
+
+    Only superusers can call CHECKPOINT.
+     */
+    public static final String CHECKPOINT = "CHECKPOINT";
+}
