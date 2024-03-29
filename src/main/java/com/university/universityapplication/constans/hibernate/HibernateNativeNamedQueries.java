c671ec46f7260afbb05cb4d2da5ec78eac8b708c
SKhuzhaev <skhuzhaev@beeline.uz>
2024-03-29 16:19:18 +0500

    upd: GET_TEACHER_AVERAGE_MARKS_QUERY was optomized

--- src/main/java/com/university/universityapplication/constans/hibernate/HibernateNativeNamedQueries.java
+++ src/main/java/com/university/universityapplication/constans/hibernate/HibernateNativeNamedQueries.java
@@ -19,26 +19,22 @@ public final class HibernateNativeNamedQueries {
     public static final String GET_TEACHER_AVERAGE_MARKS_SETTER = "GET_TEACHER_AVERAGE_MARKS_SETTER";
 
     public static final String GET_TEACHER_AVERAGE_MARKS_QUERY = """
-            SELECT COALESCE( average_mark, 0 ) AS averageMark, lessonCount,
+            SELECT COALESCE( averageMark, 0 ), lessonCount,
                 CASE
-                WHEN average_mark BETWEEN 0 AND 2 THEN 'horrible'
-                WHEN average_mark BETWEEN 2 AND 3 THEN 'normal'
-                WHEN average_mark BETWEEN 3 AND 4 THEN 'medium'
+                WHEN averageMark BETWEEN 0 AND 2 THEN 'horrible'
+                WHEN averageMark BETWEEN 2 AND 3 THEN 'normal'
+                WHEN averageMark BETWEEN 3 AND 4 THEN 'medium'
                 ELSE 'perfect'
                 END averageMarkNaming
             FROM (
-                SELECT avg( c.mark ) AS average_mark, -- средняя оценка занятия
+                SELECT avg( c.mark ) AS averageMark, -- средняя оценка занятия
                 COUNT( l.id ) AS lessonCount -- количество занятий которые провел препод
 
-                FROM university.teachers t -- находим самого учителя
-
-                INNER JOIN university.groups g ON g.teacher_id = :teacher_id -- находим все группы за которые он отвечает
+                FROM university.groups g ON g.teacher_id = :teacher_id -- находим все группы за которые он отвечает
 
                 INNER JOIN university.lessons l ON l.group_id = g.id -- находим все занятия в этих группах
 
                 INNER JOIN university.comments c ON c.lesson_id = l.id -- находим вс комметарии к этим занятиям
-
-                WHERE g.teacher_id = :teacher_id
             );
             """;
 
